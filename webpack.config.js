2226991a5f23643ad639f6303eaacddbf238505f
alvy <alvyjudy@gmail.com>
2020-12-02 03:40:22 -0500

    initial commit for set up

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require("webpack");
+
+module.exports = {
+  entry:"./src/index.js",
+  mode:"development",
+  module:{
+    rules: [
+      {
+        test:/\.js$/, 
+        exclude:/node_modules/, 
+        loader:"babel-loader",
+      }
+    ],
+  },
+  devServer:{
+    contentBase:"./dist",
+    port: 3001,
+  }
+}
