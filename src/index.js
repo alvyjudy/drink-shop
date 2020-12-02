75e851fb61d59a50a617845a6469eb981be5c5e9
alvy <alvyjudy@gmail.com>
2020-12-02 18:18:10 -0500

    reorganize index

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React, {useState} from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
+
+import {Navigation} from "./components/Navigation";
+import {TestButton} from "./TestButton";
+import {ProductsView} from "./components/ProductsView";
 
 const App = () => {
-  const [time, setTime] = useState("unknown")
   return (
     <div>
-      <p>Hello world</p>
-      <button onClick={
-        ()=>{
-          axios.get("/api/time").then(res=>{
-            setTime(res.data)
-          })
-        }
-      }>ping</button>
-      <p>{time}</p>
+      <Navigation/>
+      <TestButton/>
+      <ProductsView/>
     </div>
   )
 }
