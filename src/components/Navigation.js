2dadd0c735126578700df7f62b33646f44da5cf2
alvy <alvyjudy@gmail.com>
2020-12-02 18:18:40 -0500

    add navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Navigation.css";
+
+export const Navigation = () => {
+  return (
+    <div>
+      <div>Home</div>
+      <button>Products</button>
+      <button>My Order</button>
+    </div>
+  )
+}
+
