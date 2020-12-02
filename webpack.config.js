78c6f09524f5809197e43afeae991c27282c43f7
alvy <alvyjudy@gmail.com>
2020-12-02 18:17:57 -0500

    add css config

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
         test:/\.js$/, 
         exclude:/node_modules/, 
         loader:"babel-loader",
+      },
+      {
+        test:/\.css$/,
+        use:["style-loader", "css-loader"]
       }
     ],
   },
