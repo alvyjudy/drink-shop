4284360ccd1a615632848fd06b0de9ab8a465d04
alvy <alvyjudy@gmail.com>
2020-12-02 04:25:24 -0500

    Use a router to prefix api routes name and add a time route for testing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 
-app.get("/", (req, res) => {
+
+router.get("/", (req, res) => {
   res.send("hello world");
 })
+
+router.get("/time", (req, res)=>{
+  res.status(200).send(Date.now().toString())
+})
+
+app.use('/api', router);
 
 module.exports = app
