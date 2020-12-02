cb6681d1f6c4543a4de13c578466f8038b8d785a
alvy <alvyjudy@gmail.com>
2020-12-02 04:26:13 -0500

    add a button to test the proxy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 const App = () => {
-  return <div>Hello world</div>
+  const [time, setTime] = useState("unknown")
+  return (
+    <div>
+      <p>Hello world</p>
+      <button onClick={
+        ()=>{
+          axios.get("/api/time").then(res=>{
+            setTime(res.data)
+          })
+        }
+      }>ping</button>
+      <p>{time}</p>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
