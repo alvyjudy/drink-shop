afe6e34b98cd140163263e8652202c2a5c01dffa
alvy <alvyjudy@gmail.com>
2020-12-03 03:45:24 -0500

    add styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,10 +3,10 @@ import styles from "./Navigation.css";
 
 export const Navigation = () => {
   return (
-    <div>
-      <div>Home</div>
-      <button>Products</button>
-      <button>My Order</button>
+    <div className={styles.container}>
+      <button className={styles.button}>Products</button>
+      <div className={styles.logo}>Home</div>
+      <button className={styles.button}>My Order</button>
     </div>
   )
 }
--- src/components/ProductsView.js
+++ src/components/ProductsView.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import styles from "./ProductsView.css";
 
 export const ProductsView = (props) => {
   const [loading, setLoading] = useState(true);
@@ -11,19 +12,19 @@ export const ProductsView = (props) => {
         setLoading(false);
         setProducts(res.data);
       })
-      .catch(e=>{console.log('error')})
+      .catch(e=>{console.log('error in ajax request ProductsView')})
   },[])
 
   return (
     <div>
       {loading && <div>loading</div> }
-      {!loading && <div>{
+      {!loading && <div className={styles.ProductsView}>{
         products.map(item=>{
           return (
-            <div key={item.id}>
-              <p>name: {item.name}</p>
-              <p>id: {item.id}</p>
-              <p>price: {item.price}$</p>
+            <div key={item.id} className={styles.ProductThumbnail}>
+              <div className={styles.Img}></div>
+              <p className={styles.Name}>{item.name}</p>
+              <p className={styles.Price}>${item.price}</p>
             </div>
           )
         })
