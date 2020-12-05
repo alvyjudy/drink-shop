0bab8007b9fa7d65a30ba42742f5b8ce0b66017a
alvy <alvyjudy@gmail.com>
2020-12-05 02:00:14 -0500

    add home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
+
+import styles from "./Home.css";
 
 export const Home = () => {
-  return <div>Home</div>
+  return <div className={styles.Home}>
+    <Gallery/>
+    
+  </div>
+}
+
+const Gallery = () => {
+  const pics = ["home-1.jpg", "home-2.jpg", "home-3.jpg"];
+  const [pic, setPic] = useState(pics[0]);
+  
+  return (
+    <div className={styles.PicsContainer}>
+      <div className={styles.PicOverlay}>
+        <img src={"/assets/" + pic} className={styles.Pic}/>
+      </div>
+      
+      <div className={styles.Slider}>
+        {
+          pics.map((item,i)=>{
+            return (
+              <button
+                key={i}
+                className={styles.SlideButton}
+                onClick={e=>{
+                  e.preventDefault();
+                  setPic(item)
+                }}
+              ></button>
+          )})
+        }
+      </div>
+      <button className={styles.ShopNow}>Shop Now</button>
+    </div>
+  )
 }
