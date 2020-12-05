8890569562f26a48ba31d34509c124036f85f79c
alvy <alvyjudy@gmail.com>
2020-12-04 19:20:55 -0500

    display products and images

--- src/components/ProductsView.js
+++ src/components/ProductsView.js
@@ -5,32 +5,31 @@ import {Link} from "react-router-dom";
 import styles from "./ProductsView.css";
 
 export const ProductsView = (props) => {
-  const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     return axios.get("/api/products")
       .then(res=>{
-        setLoading(false);
+        console.log(res.data);
         setProducts(res.data);
       })
       .catch(e=>{console.log('error in ajax request ProductsView')})
-  },[])
+  }, [])
 
   return (
-    <div>
-      {loading && <div>loading</div> }
-      {!loading && <div className={styles.ProductsView}>{
-        products.map(item=>{
+    <div className={styles.ProductsView}>
+      {products.map(item=>{
           return (
             <div key={item.id} className={styles.ProductThumbnail}>
-              <Link to={"/product/" + item.id} className={styles.Img}></Link>
+              <Link to={"/product/" + item.id} className={styles.Link}>
+                <img src={"/assets/" + item.thumbnail} alt={item.thumbnail} 
+                  className={styles.Img}/>
+              </Link>
               <p className={styles.Name}>{item.name}</p>
               <p className={styles.Price}>${item.price}</p>
             </div>
           )
-        })
-      }</div>}
+        })}
     </div>
   )
 }
