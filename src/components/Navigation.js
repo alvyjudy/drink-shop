c78a23507721ebed62ce655a3dd33d1cc3916eda
alvy <alvyjudy@gmail.com>
2020-12-06 16:33:22 -0500

    added more links to navigation bar (sign up, blog)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,19 +5,35 @@ import styles from "./Navigation.css";
 
 export const Navigation = () => {
   return (
-    <div className={styles.container}>
-      <Link to="/products" className={styles.button}>
-        <span className={styles.ButtonText}>Products</span>
-      </Link>
-      <Link to="/">
-        <img className={styles.logo} 
-        src="/assets/logo.svg" 
-        alt="Home"/>
-      </Link>
-      <button className={styles.button}>
-        <span className={styles.ButtonText}>My Order</span>
-      </button>
+    <div>
+      <div className={styles.TopBannerContainer}>
+        <a className={styles.BlogLink}
+          href="https://github.com/alvyjudy/alveoli-bubble-tea-shop-frontend">
+          <img className={styles.GithubLogo}
+               src="https://cdn.iconscout.com/icon/free/png-512/github-153-675523.png"/>
+        </a>
+        <a href="https://alvyjudy-58477.medium.com/creating-a-website-for-a-bubble-tea-shop-v1-1-14ff3b397781"
+           className={styles.BlogLink}            
+        >blog</a>
+        <Link to="/sign-up"
+              className={styles.SignUpButton}
+        >Sign up</Link>
+      </div>
+      <div className={styles.container}>
+        <Link to="/products" className={styles.button}>
+          <span className={styles.ButtonText}>Products</span>
+        </Link>
+        <Link to="/">
+          <img className={styles.logo} 
+          src="/assets/logo.svg" 
+          alt="Home"/>
+        </Link>
+        <button className={styles.button}>
+          <span className={styles.ButtonText}>My Order</span>
+        </button>
+      </div>
     </div>
+    
   )
 }
 
