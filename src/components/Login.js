53bcaf9ec987115ce078f64369e62254cb126c78
alvy <alvyjudy@gmail.com>
2020-12-07 21:50:41 -0500

    add login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import {Link, useHistory} from "react-router-dom";
+import axios from "axios";
 
-export const Login = () => {
-  return <div>Login page</div>
+import styles from "./Login.css";
+
+export const Login = ({originalLink}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const history = useHistory();
+
+  return (
+    <div className={styles.AuthContainer}>
+      <div className={styles.LoginBox}>
+        <p className={styles.LoginTitle}>Login to your account</p>
+        <input className={styles.InputEmail}
+          placeholder="Email address"
+          onChange={e=>{
+            e.preventDefault();
+            setEmail(e.target.value);
+          }}
+          value={email}
+        />
+
+        <input className={styles.InputPassword}
+          placeholder="Password "
+          onChange={e=>{
+            e.preventDefault();
+            setPassword(e.target.value);
+          }}
+          value={password}
+        />
+
+        <button className={styles.LoginButton}
+          onClick={e=>{
+            e.preventDefault();
+            axios.post("/api/auth/login", {email, password})
+              .then(e=>{
+                if (e.status === 200) {
+                  localStorage.setItem("alvyBbt", e.data)
+                  history.push(originalLink || "/");
+                } else {
+                  throw Error;
+                }
+              })
+              .catch(e=>{
+                console.log(e);
+                setErrorMessage("Error")
+              })
+          }}>Login</button>
+
+        <p className={styles.ErrorMessage}>{errorMessage}</p>
+        <Link to="/auth/login">Sign up for an account!</Link>
+      </div>
+    </div>
+  )
 }
