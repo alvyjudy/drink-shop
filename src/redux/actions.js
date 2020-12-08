3426de6acd8f486c17fbc7d8c9bfa948e7c1309d
alvy <alvyjudy@gmail.com>
2020-12-08 02:05:45 -0500

    remove dead code

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -12,27 +12,5 @@ export const rmToken = (token) => {
   return {type: RM_TOKEN, token}
 }
 
-// export const loadItems = (token) => (dispatch) => {
-//   axios.get("/api/cart/get-items", {
-//     headers:{"Authorization":"Bearer "+token}
-//   }).then(res=>{
-//     dispatch({type: ADD_MINUS_ITEM, cartItems:res.data})
-//   })
-  
-// }
-
-// export const addMinusItem = (id, quantity, token) => (dispatch) => {
-//   axios.post("/api/cart/add-minus-item",[id, quantity], {
-//     headers:{
-//       "Content-Type":"application/json",
-//       "Authorization":"Bearer " + token
-//     }
-//   }).then(res=>{
-//     if (res.status === 200) {
-//       dispatch({type:ADD_MINUS_ITEM, cartItems:res.data})
-//     } else {throw Error("Error")}
-//   }).catch(e=>{
-//     console.log(e)
-//   })
 
 
