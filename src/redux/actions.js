f90ae9d3886955e1bf640eff3a61a96c69eb2678
alvy <alvyjudy@gmail.com>
2020-12-08 01:04:46 -0500

    add redux thunk

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,8 @@
+import axios from "axios";
+
 export const SET_TOKEN = "SET_TOKEN";
 export const RM_TOKEN = "RM_TOKEN";
+export const ADD_MINUS_ITEM = "ADD_MINUS_ITEM";
 
 export const setToken = (token) => {
   return {type: SET_TOKEN, token}
@@ -8,3 +11,34 @@ export const setToken = (token) => {
 export const rmToken = (token) => {
   return {type: RM_TOKEN, token}
 }
+
+export const loadItems = (items) => {
+  return {type: ADD_MINUS_ITEM, cartItems:items}
+}
+
+export const addMinusItem = (id, amount, token) => (dispatch) => {
+  axios.post("/api/cart/add-minus-item",[id, amount], {
+    headers:{
+      "Content-Type":"application/json",
+      "Authorization":"Bearer " + token
+    }
+  }).then(res=>{
+    if (res.status === 200) {
+      return "OK"
+    } else {throw Error("Error")}
+
+  }).then(res=>{
+    return axios.get("/api/cart/get-items", {
+      headers:{"Authorization":"Bearer "+token}
+    })
+
+  }).then(res=>{
+    if (res.status === 200) {
+      dispatch({type:ADD_MINUS_ITEM, cartItem:res.data})
+    } else {throw Error("Error")}
+
+  }).catch(e=>{
+    console.log(e)
+  })
+}
+
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,21 +1,42 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import thunkMiddleware from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 import {
   SET_TOKEN,
-  RM_TOKEN
+  RM_TOKEN,
+  ADD_MINUS_ITEM,
 } from "./actions";
 
-const reducer = (state={token:""}, action) => {
+
+const token = localStorage.getItem("alvybbt") || undefined;
+
+const reducer = (state={
+  token,
+  cartItems:[],
+}, action) => {
   switch (action.type) {
     case SET_TOKEN:
+      localStorage.setItem("alvybbt", action.token)
       return {token: action.token}
+
     case RM_TOKEN:
-      return {token: ""}
+      localStorage.removeItem("alvybbt")
+      return {token: undefined}
+
+    case ADD_MINUS_ITEM:
+      return {...state,
+        cartItems: action.cartItems
+      }
+
+
     default:
       return state
 
   }
 }
 
-export const store = createStore(reducer);
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+
+export const store = createStore(reducer, composedEnhancer);
 
