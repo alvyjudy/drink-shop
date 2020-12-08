b94a67ac424420dbe107e3379f2a7655fd0ec7de
alvy <alvyjudy@gmail.com>
2020-12-07 20:22:30 -0500

    add shopping cart API

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const router = express.Router();
 
 const {db} = require("./db");
 const {products}  = require("./products");
-const {signup} = require("./signup");
-const {login} = require("./login");
-const {checkLogin} = require("./checkLogin");
-const {logout} = require("./logout");
+const {signup} = require("./auth/signup");
+const {login} = require("./auth/login");
+const {checkLogin} = require("./auth/checkLogin");
+const {logout} = require("./auth/logout");
+const {getItems} = require("./cart/getItems");
+const {addMinusItem} = require("./cart/addMinusItem");
+const {removeItem} = require("./cart/removeItem");
 
 router.get("/", (req, res) => {
   res.send("hello world");
@@ -21,19 +24,66 @@ router.get("/products", products(), (req, res)=>{
   res.json(req.products)
 })
 
-router.post("/auth/signup", express.json(), signup(), login(), (req, res)=>{
+
+router.post("/auth/signup", 
+  //Content-Type: application/json
+  //Request body: {"email":<email>, "password":<password>}
+  //Response: JWT token string
+  express.json(), 
+  signup(), 
+  login(), 
+  (req, res)=>{
   res.status(200).send(req.jwtToken)
 })
 
-router.post("/auth/login", express.json(), login(), (req, res) => {
+router.post("/auth/login", 
+  //Content-Type: application/json
+  //Request body: {"email":<email>, "password":<password>}
+  express.json(), 
+  login(), 
+  (req, res) => {
   res.status(200).send(req.jwtToken)
 })
 
-router.post("/auth/logout", logout(), (req, res) => {
+router.post("/auth/logout", 
+  //Authorization: Bearer <jwtToken>
+  logout(), 
+  (req, res) => {
   res.status(200).send("Logged out")
 })
+
+router.get("/cart/get-items", checkLogin(), getItems(), (req, res)=>{
+  //Authorization: Bearer <jwtToken>
+  res.status(200).json(req.cartItems)
+})
+
+router.post("/cart/add-minus-item", 
+  //Authorization: Bearer <jwtToken>
+  //Content-Type: application/json
+  //Request body: [<itemId>, <itemCount>] (integer value)
+  express.json(), 
+  checkLogin(), 
+  addMinusItem(),
+  (req, res)=>{
+    res.status(200).send("Item has been added. Item ID:" + req.body[0]);
+  }
+)
+
+router.post("/cart/remove-item", 
+  //Authorization: Bearer <jwtToken>
+  //Content-Type: application/json
+  //Request body: [<itemId>] (integer value)
+  express.json(),
+  checkLogin(),
+  removeItem(),
+  (req, res)=>{
+    res.status(200).send("Item has been removed. Item ID: "+req.body[0])
+  }
+)
+
+
 
 
 app.use('/', router);
 
-module.exports = app
+module.exports = {app}
