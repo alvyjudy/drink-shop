cb4d28e0857629222faf9b3c2419dd178e820d7e
alvy <alvyjudy@gmail.com>
2020-12-10 04:49:42 -0500

    added database config and init script

--- src/db.js
+++ src/db.js
@@ -1,61 +1,11 @@
-function Db() {
+const {Pool} = require("pg");
 
-  this.users = new Map();
-  this.cart = new Map(); //[[<id>, <count>], [<id>, <count>]]
-  this.loggedIn = new Map();
+const pool = new Pool({
+  user: process.env.PGUSER || "dev",
+  host: process.env.PGHOST || "localhost",
+  database: process.env.PGDATABASE || "local",
+  password: process.env.PGPASSWORD || "localdev",
+  port: process.env.PGPORT || 5432
+});
 
-  this.doesEmailExist = (email) => {
-    return this.users.has(email)
-  }
-
-  this.addUser = (email, password) => {
-    this.users.set(email.toString(), password.toString());
-    this.cart.set(email, new Map())
-  }
-
-  this.doesPasswordMatch = (email, password) => {
-    return this.users.get(email) === password.toString();
-  }
-  
-
-  
-  this.loginUser = (email, token) => {
-    this.loggedIn.set(email, token)
-    
-  }
-
-  this.isUserLoggedIn = (email) => {
-    return this.loggedIn.has(email);
-  }
-
-  this.logOutUser = (email) => {
-    this.loggedIn.delete(email)
-  }
-
-
-  this.getItems = (email) => {
-    return Array.from(this.cart.get(email))
-  }
- 
-  this.addMinusItem = (item, email) => { //can be used to minus item count as well
-    const cart = this.cart.get(email);
-    if (cart.has(item[0])) {
-      const oldcount = cart.get(item[0]);
-      cart.set(item[0], Math.max(1, oldcount + item[1]))
-    } else {
-      cart.set(item[0], Math.max(1, item[1]))
-    }
-
-  }
-
-  this.removeItem = (itemId, email) => {
-    const cart = this.cart.get(email);
-    cart.delete(itemId)
-  }
-
-}
-
-const db = new Db();
-
-module.exports = {db};
-
+module.exports = {pool};
---
+++ src/dbInit.js
@@ -0,0 +1,66 @@
+const {pool} = require("./db");
+
+const SCHEMA = process.env.SCHEMA || "test";
+const dbInit = async () => {
+  await pool.query(`DROP SCHEMA IF EXISTS ${SCHEMA} CASCADE;`);
+  await pool.query(`CREATE SCHEMA ${SCHEMA};`);
+  await pool.query(`SET search_path TO ${SCHEMA};`);
+
+  await pool.query(`CREATE TABLE IF NOT EXISTS users (
+    user_id serial PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+  );`)
+
+  await pool.query(`CREATE TABLE IF NOT EXISTS session (
+    token TEXT NOT NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+  );`)
+
+  await pool.query(`CREATE TABLE IF NOT EXISTS cart (
+    item_id serial PRIMARY KEY,
+    item_catalog_id INT,
+    quantity INT, 
+    sugar INT,
+    ice INT,
+    tapioca INT,
+    pudding INT,
+    grassjelly INT,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE CASCADE
+  );`)
+
+  await pool.query(`CREATE TABLE IF NOT EXISTS orders (
+    order_id serial PRIMARY KEY,
+    address TEXT,
+    phone TEXT,
+    name TEXT,
+    payment_reference TEXT,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE CASCADE
+  );`)
+
+  await pool.query(`CREATE TABLE IF NOT EXISTS orders (
+    item_id INT,
+    item_catalog_id INT,
+    quantity INT, 
+    sugar INT,
+    ice INT,
+    tapioca INT,
+    pudding INT,
+    grassjelly INT,
+    order_id INT,
+    FOREIGN KEY (order_id) REFERENCES users(user_id)
+    ON DELETE CASCADE
+  )`)
+}
+
+module.exports = {dbInit}
+if (require.main === module) {
+  dbInit().then(res=>{
+    pool.end();
+  });
+}
