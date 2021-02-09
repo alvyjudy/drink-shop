77815d5171a44bbaecf554b53bc60c265c5cabb0
alvy <alvyjudy@gmail.com>
2021-02-09 17:01:01 -0500

    formatting

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,52 +1,59 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import {useSelector, useDispatch} from "react-redux";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 
 import styles from "./Navigation.scss";
-import {rmToken} from "../redux/actions";
+import { rmToken } from "../redux/actions";
 
 export const Navigation = () => {
-  const token = useSelector(store=>store.token);
+  const token = useSelector((store) => store.token);
   const dispatch = useDispatch();
   return (
     <div>
       <div className={styles.TopBannerContainer}>
-        <a className={styles.BlogLink}
-          href="https://github.com/alvyjudy/alveoli-bubble-tea-shop-frontend">
-          <img className={styles.GithubLogo}
-               src="https://cdn.iconscout.com/icon/free/png-512/github-153-675523.png"/>
+        <a
+          className={styles.BlogLink}
+          href="https://github.com/alvyjudy/alveoli-bubble-tea-shop-frontend"
+        >
+          <img
+            className={styles.GithubLogo}
+            src="https://cdn.iconscout.com/icon/free/png-512/github-153-675523.png"
+          />
         </a>
-        <a href="https://alvyjudy-58477.medium.com/creating-a-website-for-a-bubble-tea-shop-v1-1-14ff3b397781"
-           className={styles.BlogLink}            
-        >blog</a>
+        <a
+          href="https://alvyjudy-58477.medium.com/creating-a-website-for-a-bubble-tea-shop-v1-1-14ff3b397781"
+          className={styles.BlogLink}
+        >
+          blog
+        </a>
 
-        { !token &&
-          <Link to="/auth/sign-up"
-                className={styles.SignUpButton}
-          >Sign up</Link>
-        }
+        {!token && (
+          <Link to="/auth/sign-up" className={styles.SignUpButton}>
+            Sign up
+          </Link>
+        )}
 
-        { token && 
-          <button className={styles.SignUpButton}
-            onClick={e=>{
+        {token && (
+          <button
+            className={styles.SignUpButton}
+            onClick={(e) => {
               e.preventDefault();
               dispatch(rmToken());
               axios({
                 method: "post",
-                url:"/api/auth/logout",
-                headers: {"Authorization":"Bearer " + token}
-              })
+                url: "/api/auth/logout",
+                headers: { Authorization: "Bearer " + token },
+              });
             }}
-          >Log out</button>
-        }
-
+          >
+            Log out
+          </button>
+        )}
       </div>
       <div className={styles.container}>
-         <Link to="/">
-          <img className={styles.logo} 
-          src="/assets/logo.svg" 
-          alt="Home"/>
+        <Link to="/">
+          <img className={styles.logo} src="/assets/logo.svg" alt="Home" />
         </Link>
 
         <Link to="/products" className={styles.button}>
@@ -60,13 +67,7 @@ export const Navigation = () => {
         <Link to="/orders" className={styles.button}>
           <span className={styles.ButtonText}>Orders</span>
         </Link>
-
-
-
-        
       </div>
     </div>
-    
-  )
-}
-
+  );
+};
