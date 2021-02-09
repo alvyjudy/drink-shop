ce17c30180e4aa56a3220c9cecfb1dd40fa9affa
alvy <alvyjudy@gmail.com>
2021-02-09 18:56:48 -0500

    code style

--- client/src/components/ProductsView.js
+++ client/src/components/ProductsView.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import styles from "./ProductsView.scss";
 
@@ -8,27 +8,34 @@ export const ProductsView = (props) => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    return axios.get("/api/products")
-      .then(res=>{
+    return axios
+      .get("/api/products")
+      .then((res) => {
         setProducts(res.data);
       })
-      .catch(e=>{console.log('error in ajax request ProductsView')})
-  }, [])
+      .catch((e) => {
+        console.log("error in ajax request ProductsView");
+      });
+  }, []);
 
+  console.log("products", products);
   return (
     <div className={styles.ProductsView}>
-      {products.map(item=>{
-          return (
-            <div key={item.id} className={styles.ProductThumbnail}>
-              <Link to={"/product/" + item.id} className={styles.Link}>
-                <img src={"/assets/" + item.thumbnail} alt={item.thumbnail} 
-                  className={styles.Img}/>
-              </Link>
-              <p className={styles.Name}>{item.name}</p>
-              <p className={styles.Price}>${item.price}</p>
-            </div>
-          )
-        })}
+      {products.map((item) => {
+        return (
+          <div key={item.id} className={styles.ProductThumbnail}>
+            <Link to={"/product/" + item.id} className={styles.Link}>
+              <img
+                src={"/assets/" + item.thumbnail}
+                alt={item.thumbnail}
+                className={styles.Img}
+              />
+            </Link>
+            <p className={styles.Name}>{item.name}</p>
+            <p className={styles.Price}>${item.price}</p>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
