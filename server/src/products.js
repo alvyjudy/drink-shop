2cc38aa2eb2bb1f3a6354635bacd70fa83c59d64
alvy <alvyjudy@gmail.com>
2021-02-09 18:56:40 -0500

    fix product catalog naming

--- server/src/products.js
+++ server/src/products.js
@@ -1,40 +1,55 @@
 const products = () => (req, res, next) => {
   req.products = [
-    {id:1, name: "classic", price: 5,
+    {
+      id: 1,
+      name: "classic",
+      price: 5,
       thumbnail: "classic-thumbnail.jpg",
-      mainPic:"classic-main.jpg",
-      sidePics:["classic-side-1.jpg","classic-side-2.jpg","classic-side-3.jpg"]
+      mainPic: "1-main.jpg",
+      sidePics: ["1-side-1.jpg", "1-side-2.jpg", "1-side-3.jpg"],
     },
-    {id:2, name: "original", price: 3,
-      thumbnail: "original-thumbnail.jpg",
-      mainPic:"original-main.jpg",
-      sidePics:["original-side-1.jpg","original-side-2.jpg","original-side-3.jpg"]
+    {
+      id: 2,
+      name: "original",
+      price: 3,
+      thumbnail: "2-thumbnail.jpg",
+      mainPic: "2-main.jpg",
+      sidePics: ["2-side-1.jpg", "2-side-2.jpg", "2-side-3.jpg"],
     },
-    {id:3, name: "garden", price: 4,
-      thumbnail: "garden-thumbnail.jpg",
-      mainPic:"garden-main.jpg",
-      sidePics:["garden-side-1.jpg","garden-side-2.jpg","garden-side-3.jpg"]
+    {
+      id: 3,
+      name: "garden",
+      price: 4,
+      thumbnail: "3-thumbnail.jpg",
+      mainPic: "3-main.jpg",
+      sidePics: ["3-side-1.jpg", "3-side-2.jpg", "3-side-3.jpg"],
     },
-    {id:4, name:"brown-sugar", price: 9,
-      thumbnail:"brown-sugar-thumbnail.jpg",
-      mainPic:"brown-sugar-main.jpg",
-      sidePics:["brown-sugar-side-1.jpg","brown-sugar-side-2.jpg","brown-sugar-side-3.jpg"]
-  
+    {
+      id: 4,
+      name: "brown-sugar",
+      price: 9,
+      thumbnail: "4-thumbnail.jpg",
+      mainPic: "4-main.jpg",
+      sidePics: ["4-side-1.jpg", "4-side-2.jpg", "4-side-3.jpg"],
     },
-    {id:5, name: "oreo", price: 4,
-    thumbnail: "oreo-thumbnail.jpg",
-    mainPic:"oreo-main.jpg",
-    sidePics:["oreo-side-1.jpg","oreo-side-2.jpg","oreo-side-3.jpg"]
-  },
-  {id:6, name: "passion-fruit", price: 4,
-  thumbnail: "passion-fruit-thumbnail.jpg",
-  mainPic:"passion-fruit-main.jpg",
-  sidePics:["passion-fruit-side-1.jpg","passion-fruit-side-2.jpg","passion-fruit-side-3.jpg"]
-},
+    {
+      id: 5,
+      name: "oreo",
+      price: 4,
+      thumbnail: "5-thumbnail.jpg",
+      mainPic: "5-main.jpg",
+      sidePics: ["5-side-1.jpg", "5-side-2.jpg", "5-side-3.jpg"],
+    },
+    {
+      id: 6,
+      name: "passion-fruit",
+      price: 4,
+      thumbnail: "6-thumbnail.jpg",
+      mainPic: "6-main.jpg",
+      sidePics: ["6-side-1.jpg", "6-side-2.jpg", "6-side-3.jpg"],
+    },
+  ];
+  next();
+};
 
-
-  ]
-  next()
-}
-
-module.exports = {products}
+module.exports = { products };
