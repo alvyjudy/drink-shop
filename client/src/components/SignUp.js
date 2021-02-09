3e6db7838216787712f0ad5297dd18112d49b05e
alvy <alvyjudy@gmail.com>
2021-02-09 18:20:40 -0500

    sign up page styles

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,66 +1,96 @@
-import React, {useState} from "react";
-import {Link, useHistory} from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
-import {useDispatch} from "react-redux"
+import { useDispatch } from "react-redux";
 
 import styles from "./SignUp.scss";
-import {setToken} from "../redux/actions";
+import { setToken } from "../redux/actions";
 
-export const SignUp = ({originalLink}) => {
+export const SignUp = ({ originalLink }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [signupPage, setSignupPage] = useState(true);
   const history = useHistory();
   const dispatch = useDispatch();
 
   return (
     <div className={styles.AuthContainer}>
-      <form className={styles.SignUpBox}
-        onSubmit={e=>{
+      <form
+        className={styles.SignUpBox}
+        onSubmit={(e) => {
           e.preventDefault();
-          axios.post("/api/auth/signup", {email, password})
-            .then(e=>{
-              if (e.status === 200) {
-                dispatch(setToken(e.data.token));
-                history.push(originalLink || "/");
-              } else {
-                throw Error;
-              }
-            })
-            .catch(e=>{
-              console.log(e);
-              setErrorMessage("Error")
-            })
+          if (signupPage) {
+            axios
+              .post("/api/auth/signup", { email, password })
+              .then((e) => {
+                if (e.status === 200) {
+                  dispatch(setToken(e.data.token));
+                  history.push(originalLink || "/");
+                } else {
+                  throw Error;
+                }
+              })
+              .catch((e) => {
+                console.log(e);
+                setErrorMessage("Error");
+              });
+          }
         }}
       >
-        <p className={styles.SignUpTitle}>Sign up with us</p>
-        <input className={styles.InputEmail}
+        <div className={styles.tab}>
+          <button
+            type="button"
+            className={
+              signupPage ? styles["tabTitle--active"] : styles.tabTitle
+            }
+            onClick={() => {
+              setSignupPage(true);
+            }}
+          >
+            Sign up
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              setSignupPage(false);
+            }}
+            className={
+              signupPage ? styles.tabTitle : styles["tabTitle--active"]
+            }
+          >
+            Log in
+          </button>
+        </div>
+
+        <input
+          className={styles.inputField}
           required
           type="email"
           placeholder="Email address"
-          onChange={e=>{
+          onChange={(e) => {
             setEmail(e.target.value);
           }}
           value={email}
         />
 
-        <input className={styles.InputPassword}
+        <input
+          className={styles.inputField}
           required
           type="password"
           placeholder="Password"
-          onChange={e=>{
+          onChange={(e) => {
             setPassword(e.target.value);
           }}
           value={password}
         />
 
-        <button type="submit" className={styles.SignUpButton}>
-          Sign up
+        <button type="submit" className={styles.signUpButton}>
+          {signupPage ? "Sign up" : "Log in"}
         </button>
 
         <p className={styles.ErrorMessage}>{errorMessage}</p>
-        <Link to="/auth/login">Have an account? Login instead</Link>
       </form>
     </div>
-  )
-}
+  );
+};
