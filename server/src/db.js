83ad7b91a6b1b07784d04ac48584d623604565ce
alvy <alvyjudy@gmail.com>
2021-02-10 00:43:32 -0500

    node_env

--- server/src/db.js
+++ server/src/db.js
@@ -1,37 +1,38 @@
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 
-const connectionConfig = (
-  process.env.ENV === "production" || 
-  process.env.ENV === "staging"
-  ) ? 
-  {connectionString: process.env.DATABASE_URL} :
-  {
-    user: "dev",
-    host: "localhost",
-    database: "local",
-    password: "localdev",
-    port: 5432
-  };
+const connectionConfig =
+  process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging"
+    ? { connectionString: process.env.DATABASE_URL }
+    : {
+        user: "dev",
+        host: "localhost",
+        database: "local",
+        password: "localdev",
+        port: 5432,
+      };
 
-const pool = new Pool(
-  connectionConfig
-  );
+const pool = new Pool(connectionConfig);
 
-module.exports = {pool: {
-  query: (text, params) => {
-    //console.log(text);
-    return pool.query(text, params).then(res=>{
-      //console.log(res.rows)
-      return res
-    }).catch(e=>{
-      console.log("SQL query error for statement", text,"\n", e)
-      return e;
-    });
+module.exports = {
+  pool: {
+    query: (text, params) => {
+      //console.log(text);
+      return pool
+        .query(text, params)
+        .then((res) => {
+          //console.log(res.rows)
+          return res;
+        })
+        .catch((e) => {
+          console.log("SQL query error for statement", text, "\n", e);
+          return e;
+        });
+    },
+    end: () => {
+      return pool.end();
+    },
+    connect: () => {
+      return pool.connect();
+    },
   },
-  end:()=>{
-    return pool.end()
-  },
-  connect: () => {
-    return pool.connect();
-  }
-}};
+};
